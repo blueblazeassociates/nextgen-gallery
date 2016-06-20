fada1f0894ab7e76c0140bf5f363896d49074f30
Ed Gifford <egifford@blueblazeassociates.com>
2016-06-20 11:18:04 -0400

    Bringing in changes from 2.1.31.

--- products/photocrati_nextgen/modules/ngglegacy/nggallery.php
+++ products/photocrati_nextgen/modules/ngglegacy/nggallery.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * Modified by Blue Blaze Associates, LLC
+ *
+ * Changes include:
+ *
+ * BEGIN egifford 2015_11_17: Fixing unknown/uninitialized index PHP error.
+ */
 
 // Stop direct call
 if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('You are not allowed to call this page directly.'); }
@@ -71,8 +78,11 @@ class nggLoader
 		} else {
 
 			// Add MRSS to wp_head
-			if ( $this->options['useMediaRSS'] )
+// BEGIN egifford 2015_11_17: Fixing unknown/uninitialized index PHP error.
+//			if ( $this->options['useMediaRSS'] )
+			if ( isset( $this->options['useMediaRSS'] ) && $this->options['useMediaRSS'] )
 				add_action('wp_head', array('nggMediaRss', 'add_mrss_alternate_link'));
+// END egifford 2015_11_17
 
 		}
 	}
